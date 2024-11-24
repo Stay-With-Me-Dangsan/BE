92c378750aa448f19bea453761678450b6234127
hyein <hy88101270@gmail.com>
2024-11-24 12:45:41 +0900

    [Init]: init

---
+++ src/test/java/stay/with/me/StayWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package stay.with.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
