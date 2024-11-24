74e15943fd3ea50210c525c0f04cb3f63820dcd5
hyein <hy88101270@gmail.com>
2024-11-24 12:45:41 +0900

    [Init]: init

---
+++ src/main/java/stay/with/me/StayWithMeApplication.java
@@ -0,0 +1,13 @@
+package stay.with.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayWithMeApplication.class, args);
+    }
+
+}
