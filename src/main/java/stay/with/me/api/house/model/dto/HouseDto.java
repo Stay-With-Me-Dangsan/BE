c36a583fbacac966c3936d0906b51e1bfa620e99
Hyein Kim <hyeinkim@penta.co.kr>
2024-11-25 09:22:07 +0900

    [Add]: 기본 구조

---
+++ src/main/java/stay/with/me/api/house/model/dto/HouseDto.java
@@ -0,0 +1,10 @@
+package stay.with.me.api.house.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HouseDto {
+
+}
---
+++ src/main/java/stay/with/me/api/house/service/impl/HouseServiceImpl.java
@@ -0,0 +1,13 @@
+package stay.with.me.api.house.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import stay.with.me.api.house.model.mapper.HouseMapper;
+
+@Service
+@RequiredArgsConstructor
+public class HouseServiceImpl {
+
+//    private final HouseMapper houseMapper;
+
+}
