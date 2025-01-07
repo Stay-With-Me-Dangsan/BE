af26576092bcc7c7fd94f7291a1bd182a64a475f
hyein <hy88101270@gmail.com>
2025-01-07 22:09:43 +0900

    [Add]: 건물 상세 생성 및 단 건 조회

--- src/main/java/stay/with/me/api/model/mapper/HouseMapper.java
+++ src/main/java/stay/with/me/api/model/mapper/HouseMapper.java
@@ -1,8 +1,20 @@
 package stay.with.me.api.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import stay.with.me.api.model.dto.HouseDto;
+
+import java.util.List;
 
 @Mapper
 public interface HouseMapper {
 
+    List<HouseDto> getDetail(HouseDto param);
+
+    List<HouseDto> getDetails(List<HouseDto> params);
+
+    void createDetail(HouseDto param);
+
+    void updateDetail(HouseDto param);
+
+    void deleteDetail(HouseDto param);
 }
--- src/main/java/stay/with/me/api/service/HouseService.java
+++ src/main/java/stay/with/me/api/service/HouseService.java
@@ -1,5 +1,19 @@
 package stay.with.me.api.service;
 
+import stay.with.me.api.model.dto.HouseDto;
+
+import java.util.List;
+
 public interface HouseService {
+
+    List<HouseDto> getDetail(HouseDto param);
+
+    List<HouseDto> getDetails(List<HouseDto> params);
+
+    void createDetail(HouseDto param);
+
+    void updateDetail(HouseDto param);
+
+    void deleteDetail(HouseDto param);
 
 }
