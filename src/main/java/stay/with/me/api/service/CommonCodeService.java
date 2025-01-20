906169801ec5c307911f8c567c293dad37cbb4f5
hyein <hy88101270@gmail.com>
2025-01-20 21:25:26 +0900

    [Add]: 건물 및 공통코드 API 추가

---
+++ src/main/java/stay/with/me/api/service/CommonCodeService.java
@@ -0,0 +1,17 @@
+package stay.with.me.api.service;
+
+import stay.with.me.api.model.dto.CommonCodeDto;
+
+import java.util.List;
+
+public interface CommonCodeService {
+
+    List<CommonCodeDto> get(String groupId, String key) throws Exception;
+
+    int create(CommonCodeDto param) throws Exception;
+
+    int update(CommonCodeDto param) throws Exception;
+
+    boolean delete(int commonCodeId) throws Exception;
+
+}
