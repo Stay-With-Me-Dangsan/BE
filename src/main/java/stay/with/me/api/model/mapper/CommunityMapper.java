40d07674c99fbaa48fc7fb25894bce3c4d924ee8
hyein <hy88101270@gmail.com>
2025-02-18 14:08:09 +0900

    [Add]: 커뮤니티 테스트 코드 및 cors 설정

---
+++ src/main/java/stay/with/me/api/model/mapper/CommunityMapper.java
@@ -0,0 +1,14 @@
+package stay.with.me.api.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import stay.with.me.api.model.dto.CommunityDto;
+
+import java.util.List;
+
+@Mapper
+public interface CommunityMapper {
+
+    List<CommunityDto> getPostgresChat(String district, String lastConDt);
+
+    void saveChat();
+}
