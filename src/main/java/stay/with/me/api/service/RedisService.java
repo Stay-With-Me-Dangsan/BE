e66b50642d8504fa5c4cdd878f78488c6e918942
hyein <hy88101270@gmail.com>
2025-02-19 09:22:09 +0900

    [Refactor]: redis 저장 방식 테스트

--- src/main/java/stay/with/me/api/service/RedisService.java
+++ src/main/java/stay/with/me/api/service/RedisService.java
@@ -2,15 +2,12 @@ package stay.with.me.api.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import stay.with.me.api.model.dto.CommunityDto;
-import stay.with.me.api.service.impl.CommunityServiceImpl;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -20,23 +17,18 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
     private static final String KEY = "chat";
-    private HashOperations<String, String, Object> hashOperations;
     private final ObjectMapper objectMapper;
-    private static final Logger logger = LoggerFactory.getLogger(CommunityServiceImpl.class);
-
-    @PostConstruct
-    private void init() {
-        hashOperations = redisTemplate.opsForHash();
-    }
+    private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
 
     public void saveChat(List<CommunityDto> chatList, String district) {
         for(CommunityDto chat : chatList) {
             String chatKey = KEY + ":" + district + ":" + chat.getChatId();
             try {
                 String json = objectMapper.writeValueAsString(chat);
+                logger.info(">>>>> json: {}", json);
                 redisTemplate.opsForValue().set(chatKey, json, 30, TimeUnit.MINUTES);
             } catch(JsonProcessingException e) {
-                logger.error("json 변환 실패: {}", chat, e);
+                logger.error(">>>>> json 변환 실패: {}", chat, e);
             }
             redisTemplate.expire(chatKey, 30, TimeUnit.MINUTES);
         }
