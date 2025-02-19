bb789120201fdd8ab77e94fa0ef217ef66123d5e
hyein <hy88101270@gmail.com>
2025-02-19 09:55:24 +0900

    [Refactor]: redis 저장 방식 변경(json)

--- src/main/java/stay/with/me/api/service/RedisService.java
+++ src/main/java/stay/with/me/api/service/RedisService.java
@@ -1,10 +1,6 @@
 package stay.with.me.api.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import stay.with.me.api.model.dto.CommunityDto;
@@ -17,19 +13,11 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
     private static final String KEY = "chat";
-    private final ObjectMapper objectMapper;
-    private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
 
     public void saveChat(List<CommunityDto> chatList, String district) {
         for(CommunityDto chat : chatList) {
             String chatKey = KEY + ":" + district + ":" + chat.getChatId();
-            try {
-                String json = objectMapper.writeValueAsString(chat);
-                logger.info(">>>>> json: {}", json);
-                redisTemplate.opsForValue().set(chatKey, json, 30, TimeUnit.MINUTES);
-            } catch(JsonProcessingException e) {
-                logger.error(">>>>> json 변환 실패: {}", chat, e);
-            }
+            redisTemplate.opsForValue().set(chatKey, chat, 30, TimeUnit.MINUTES);
             redisTemplate.expire(chatKey, 30, TimeUnit.MINUTES);
         }
     }
